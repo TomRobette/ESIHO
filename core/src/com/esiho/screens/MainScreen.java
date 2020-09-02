814fd5e105f2c37157a9122e0d80bfe3db5800f9
TomRobette <robettetom@gmail.com>
2020-09-02 11:11:25 +0200

    ESIHO-1.1.2 No Save To Maps

--- core/src/com/esiho/screens/MainScreen.java
+++ core/src/com/esiho/screens/MainScreen.java
@@ -109,7 +109,7 @@ public class MainScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 1, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
